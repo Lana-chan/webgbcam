815ecf8824237f23917ceea6a9d6401598516362
Erin Pinheiro <hello@mynameiser.in>
2020-10-25 02:19:10 -0300

    serviceworker for offline usage

---
+++ sw.js
@@ -0,0 +1,44 @@
+const cacheName = 'webgbcam-v1'
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open(cacheName).then(function(cache) {
+     return cache.addAll([
+			 '.',
+       'index.html',
+       'style.css',
+       'app.js',
+       'bg.png',
+       'mac-frame.png',
+       'ui-capture.png',
+       'ui-settings.png',
+       'ui-main.png'
+     ]);
+   })
+ );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener('fetch', event => {
+	// Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(cacheName).then(cache => {
+          return fetch(event.request).then(response => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
